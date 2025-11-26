8c6cb9475dfcb4310a50332907a86af0e81dfab5
Driann's <114148680+Driannm@users.noreply.github.com>
2025-11-26 20:09:45 +0700

    feat : simple login page using shadcn login-form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,56 +1,20 @@
-"use client";
+import { LoginForm } from "@/components/login-form"
 
-import { useState } from "react";
-
-export default function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-
-  async function handleLogin(e: any) {
-    e.preventDefault();
-    setError("");
-
-    const res = await fetch("/api/auth/login", {
-      method: "POST",
-      body: JSON.stringify({ email, password }),
-    });
-
-    if (!res.ok) {
-      setError("Email atau password salah");
-      return;
-    }
-
-    window.location.href = "/dashboard";
-  }
-
+export default function Page() {
   return (
-    <div style={{ maxWidth: 400, margin: "100px auto" }}>
-      <h2 style={{ fontSize: 28, marginBottom: 20 }}>Login</h2>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm space-y-4">
+        
+        {/* TEKS DI ATAS FORM */}
+        <div className="text-center">
+          <h1 className="text-2xl font-semibold">CS Assistance</h1>
+          <p className="text-sm text-muted-foreground">
+            Masuk untuk melanjutkan
+          </p>
+        </div>
 
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="border p-2 w-full mb-3"
-        />
-
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="border p-2 w-full mb-3"
-        />
-
-        {error && <p className="text-red-500">{error}</p>}
-
-        <button className="bg-blue-500 text-white px-4 py-2 w-full rounded">
-          Login
-        </button>
-      </form>
+        <LoginForm />
+      </div>
     </div>
-  );
+  )
 }
